78cfa249e7d987bd1ddc0436cb128e4907fed7e2
Lucas Queiroz <lqvieira7@gmail.com>
2026-04-20 13:32:47 -0300

    feat: configuração inicial do app.js

---
+++ backend/src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const clienteRoutes = require('./clienteRoutes/');
+const boletoRoutes = require('./routes/boletoRoutes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/clientes', clienteRoutes);
+app.use('/boletos', boletoRoutes);
+
+module.exports = app;
