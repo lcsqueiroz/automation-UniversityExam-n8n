05e490339bdf2d5324add78f648a5ca220c4eb0b
Lucas Queiroz <lqvieira7@gmail.com>
2026-04-21 15:42:11 -0300

    feat: app da aplicação

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,19 @@
-import Button from './components/atoms/Button/Button';
-import Label from './components/atoms/Input/Input';
-import Badge from './components/atoms/Badge/Badge';
-import Avatar from './components/atoms/Avatar/Avatar';
-import StatCard from './components/molecules/StatCard/StatCard';
-import SearchBar from './components/molecules/SearchBar/SearchBar';
-import FormGroup from './components/molecules/FormGroup/FormGroup';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Topbar from './components/organisms/Topbar/Topbar';
+import Dashboard from './pages/Dashboard/Dashboard';
+import Clientes from './pages/Clientes/Clientes';
+import Boletos from './pages/Boletos/Boletos';
 
 function App() {
   return (
-    // teste dos atoms
-    <>
-      <Button variant="secondary">Teste</Button>
-      <Label placeholder="Digite algo"></Label>
-      {/* teste de badges */}
-      <Badge status={'enviado'}></Badge>
-      <Badge status={'pago'}></Badge>
-      <Badge status={'pendente'}></Badge>
-      <Avatar name={'Lucas Queiroz Vieira'}></Avatar>
-      <StatCard label={'teste'} value={'outro teste'} sub={'testee'}></StatCard>
-      <StatCard label={'teste'} value={'outro teste'} sub={'testee'}></StatCard>
-      <StatCard label={'teste'} value={'outro teste'} sub={'testee'}></StatCard>
-      <SearchBar></SearchBar>
-    </>
+    <BrowserRouter>
+      <Topbar />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/clientes" element={<Clientes />} />
+        <Route path="/boletos" element={<Boletos />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
